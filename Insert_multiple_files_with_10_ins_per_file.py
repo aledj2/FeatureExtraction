67a071b4d32d28a6f7199f20297de41bb92c55a9
aledj2 <aledjones@nhs.net>
2015-02-13 17:31:17 +0000

    Inserts the file name. SQL code to create db

--- Insert_multiple_files_with_10_ins_per_file.py
+++ Insert_multiple_files_with_10_ins_per_file.py
@@ -3,25 +3,29 @@ import MySQLdb
 import math
 import os
 
-
 class Getfile():
-    #specify the file and folder
+    #specify the folder.  
     #chosenfolder = 'C:\Users\user\workspace\Parse_FE_File' #laptop
     chosenfolder = "C:\Users\Aled\workspace\FeatureExtraction\FEFiles" #PC
     
+    # Create an array to store all the files in. 
     chosenfiles=[]
+    # loop through this folder and add any txt files to this array 
     for file in os.listdir(chosenfolder):
         if file.endswith(".txt"):
             #print (file)
             chosenfiles.append(file)
 
-
 class extractData():  
     def feedfile(self,filein):
+        #filein is the file name from the array filled in above. one filename is supplied.   
+        filein=filein
+        
         file2open= Getfile.chosenfolder+"\\"+filein
-        #print filein
+        
         #open file
         wholefile=open(file2open,'r')
+        
         #create arrays to hold results from each section of FE file.
         feparams=[]
         stats=[]
@@ -69,12 +73,13 @@ class extractData():
                 i.insert(8,splitgenloc[0])
                 i.insert(9,splitgenloc[1])
                 i.insert(10,splitgenloc[2])
-
-        ins_feparams().insert_feparams(feparams,stats,features)
+        # pass the three arrays into the insert params class
+        ins_feparams().insert_feparams(feparams,stats,features,filein)
         
 
 class ins_feparams():
-    def insert_feparams(self,feparams_listin,stats_listin,features_listin):
+    #this function recieves the three arrays filled above. 
+    def insert_feparams(self,feparams_listin,stats_listin,features_listin,filein):
         #need to create a copy of FEPARAMS from above to modify (using list()).
         allfeparams=list(feparams_listin)
         #use pop to remove the newline from final element in list
@@ -83,30 +88,34 @@ class ins_feparams():
         allfeparams.append(no_newline)
         #need to remove the first entry in the list ('DATA') as this is not needed in db.#use remove to remove 'DATA' (remove function removes the first existence of that entry in the list)
         allfeparams.remove('DATA')
-                
+        
+        #take filename to add to database below
+        filename=filein
+                        
         #open connection to database and run SQL insert statement
         db=MySQLdb.Connect(host="localhost",port=3307, user ="aled",passwd="aled",db="featextr")
         cursor=db.cursor()
-        feparams_ins_statement="""insert into feparam (ProtocolName,ProtocolDate,Scan_ScannerName,Scan_NumChannels,Scan_Date,Scan_MicronsPerPixelX,Scan_MicronsPerPixelY,Scan_OriginalGUID,Scan_NumScanPass,Grid_Name,Grid_Date,Grid_NumSubGridRows,Grid_NumSubGridCols,Grid_NumRows,Grid_NumCols,Grid_RowSpacing,Grid_ColSpacing,Grid_OffsetX,Grid_OffsetY,Grid_NomSpotWidth,Grid_NomSpotHeight,Grid_GenomicBuild,FeatureExtractor_Barcode,FeatureExtractor_Sample,FeatureExtractor_ScanFileName,FeatureExtractor_ArrayName,FeatureExtractor_ScanFileGUID,FeatureExtractor_DesignFileName,FeatureExtractor_ExtractionTime,FeatureExtractor_UserName,FeatureExtractor_ComputerName,FeatureExtractor_Version,FeatureExtractor_IsXDRExtraction,FeatureExtractor_ColorMode,FeatureExtractor_QCReportType,DyeNorm_NormFilename,DyeNorm_NormNumProbes,Grid_IsGridFile) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
-        try:
-            
-            cursor.execute(feparams_ins_statement,(allfeparams[0],allfeparams[1],allfeparams[2],allfeparams[3],allfeparams[4],allfeparams[5],allfeparams[6],allfeparams[7],allfeparams[8],allfeparams[9],allfeparams[10],allfeparams[11],allfeparams[12],allfeparams[13],allfeparams[14],allfeparams[15],allfeparams[16],allfeparams[17],allfeparams[18],allfeparams[19],allfeparams[20],allfeparams[21],allfeparams[22],allfeparams[23],allfeparams[24],allfeparams[25],allfeparams[26],allfeparams[27],allfeparams[28],allfeparams[29],allfeparams[30],allfeparams[31],allfeparams[32],allfeparams[33],allfeparams[34],allfeparams[35],allfeparams[36],allfeparams[37]))
+        feparams_ins_statement="""insert into feparam (FileName,ProtocolName,ProtocolDate,Scan_ScannerName,Scan_NumChannels,Scan_Date,Scan_MicronsPerPixelX,Scan_MicronsPerPixelY,Scan_OriginalGUID,Scan_NumScanPass,Grid_Name,Grid_Date,Grid_NumSubGridRows,Grid_NumSubGridCols,Grid_NumRows,Grid_NumCols,Grid_RowSpacing,Grid_ColSpacing,Grid_OffsetX,Grid_OffsetY,Grid_NomSpotWidth,Grid_NomSpotHeight,Grid_GenomicBuild,FeatureExtractor_Barcode,FeatureExtractor_Sample,FeatureExtractor_ScanFileName,FeatureExtractor_ArrayName,FeatureExtractor_ScanFileGUID,FeatureExtractor_DesignFileName,FeatureExtractor_ExtractionTime,FeatureExtractor_UserName,FeatureExtractor_ComputerName,FeatureExtractor_Version,FeatureExtractor_IsXDRExtraction,FeatureExtractor_ColorMode,FeatureExtractor_QCReportType,DyeNorm_NormFilename,DyeNorm_NormNumProbes,Grid_IsGridFile) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+        try:           
+            cursor.execute(feparams_ins_statement,(str(filename),allfeparams[0],allfeparams[1],allfeparams[2],allfeparams[3],allfeparams[4],allfeparams[5],allfeparams[6],allfeparams[7],allfeparams[8],allfeparams[9],allfeparams[10],allfeparams[11],allfeparams[12],allfeparams[13],allfeparams[14],allfeparams[15],allfeparams[16],allfeparams[17],allfeparams[18],allfeparams[19],allfeparams[20],allfeparams[21],allfeparams[22],allfeparams[23],allfeparams[24],allfeparams[25],allfeparams[26],allfeparams[27],allfeparams[28],allfeparams[29],allfeparams[30],allfeparams[31],allfeparams[32],allfeparams[33],allfeparams[34],allfeparams[35],allfeparams[36],allfeparams[37]))
             db.commit()
-            #print "feparams insert was a success"
-            arrayID=cursor.lastrowid
             
+            #return the arrayID for the this array (automatically retrieve the Feature_ID from database) 
+            arrayID=cursor.lastrowid
         except:
             db.rollback
             print "fail - unable to enter feparams information"
         db.close
         
+        # pass to the ins stats function the stats_listin and features_listin (neither have been used in this module) and the array_ID created on the insert.
         ins_stats().insert_stats(stats_listin,arrayID,features_listin)
         
 class ins_stats():
-    def insert_stats(self,statslist,array_ID,features_listin):
-        #create a copy of the stats array created above using list().
-        all_stats=list(statslist)
+    def insert_stats(self,statslistin,array_ID,features_listin):
+        #create a copy of the stats array and arrayID.
+        all_stats=list(statslistin)
         arrayID=array_ID
+        
         #remove final element and remove new line
         stats_with_newline=all_stats.pop()
         no_newline=stats_with_newline.replace('\n','')
@@ -127,9 +136,58 @@ class ins_stats():
             print "fail - unable to enter stats information"
         db.close
         arrayID=array_ID
-        run_ins_statements().run_ins_statements (features_listin,arrayID)
         
+        # pass the features list and array ID into the run_ins statement module 
+        run_ins_statements().run_ins_statements(features_listin,arrayID)
           
+       
+class run_ins_statements:
+    def run_ins_statements(self,features_listin,arrayID):       
+        # it is quicker to run fewer insert statements so 10 insert statements are created.
+        # create a copy of features array 
+        all_features=list(features_listin)
+            
+        #calculate number of features
+        no_of_probes=len(all_features)
+        
+            
+        # use the array_ID that is returned from the insert of feparams.       
+        Array_ID=arrayID
+        #Array_ID=1
+            
+        # using the total number of probes break down into ten subsets. use math.ceil to round up to ensure all probes are included.    
+        subset0=0
+        subset1=int(math.ceil((no_of_probes/10)))
+        subset2=subset1*2
+        subset3=subset1*3
+        subset4=subset1*4
+        subset5=subset1*5
+        subset6=subset1*6
+        subset7=subset1*7
+        subset8=subset1*8
+        subset9=subset1*9
+        
+        #Rename the create_insert_statement class       
+        Create_ins_statements= create_ins_statements()
+                
+        #call the looper function within this class and pass it the subset numbers, allfeatures array and array ID
+        Create_ins_statements.looper(subset0,subset1,all_features,Array_ID)
+        Create_ins_statements.looper(subset1,subset2,all_features,Array_ID)
+        Create_ins_statements.looper(subset2,subset3,all_features,Array_ID)
+        Create_ins_statements.looper(subset3,subset4,all_features,Array_ID)
+        Create_ins_statements.looper(subset4,subset5,all_features,Array_ID)
+        Create_ins_statements.looper(subset5,subset6,all_features,Array_ID)
+        Create_ins_statements.looper(subset6,subset7,all_features,Array_ID)
+        Create_ins_statements.looper(subset7,subset8,all_features,Array_ID)
+        Create_ins_statements.looper(subset8,subset9,all_features,Array_ID)
+        Create_ins_statements.looper(subset9,no_of_probes,all_features,Array_ID)
+        
+        # Once all SQL statements have been created feed these into the insert features module 
+        insert_features().insert_features(Create_ins_statements.insertstatements,Create_ins_statements.insertstatementnames)
+        
+
+
+#create class which builds SQL statements
 class create_ins_statements():
     #An insert statement which is appended to in the below looper function    
     baseinsertstatement = "INSERT INTO FEATURES(Array_ID,FeatureNum,Row,Col,SubTypeMask,ControlType,ProbeName,SystematicName,Chromosome,Start,Stop,PositionX,PositionY,LogRatio,LogRatioError,PValueLogRatio,gProcessedSignal,rProcessedSignal,gProcessedSigError,rProcessedSigError,gMedianSignal,rMedianSignal,gBGMedianSignal,rBGMedianSignal,gBGPixSDev,rBGPixSDev,gIsSaturated,rIsSaturated,gIsFeatNonUnifOL,rIsFeatNonUnifOL,gIsBGNonUnifOL,rIsBGNonUnifOL,gIsFeatPopnOL,rIsFeatPopnOL,gIsBGPopnOL,rIsBGPopnOL,IsManualFlag,gBGSubSignal,rBGSubSignal,gIsPosAndSignif,rIsPosAndSignif,gIsWellAboveBG,rIsWellAboveBG,SpotExtentX,gBGMeanSignal,rBGMeanSignal) values "
@@ -140,12 +198,16 @@ class create_ins_statements():
     #print self.insertstatementnames
         
     def looper(self,start,stop,allfeatures,arrayID):
+        #receives the start,stop, all features and arrayID from run_ins_statements 
         """This takes the start and stop of each subset and loops through the all_features list modifying and appending to a SQL statement and then adding to dictionary """
         #create a copy of the insert statement
         insstatement=self.baseinsertstatement
+        
+        #take the allfeatures array and array ID that is given to module  
         all_features=allfeatures
         Array_ID=arrayID
-        #loop through all features in range of lines given (provided when function is called below) 
+        
+        #loop through all_features array in range of lines given (provided when function is called below) 
         #NB when using range stop is not selected eg range(1-10) will select numbers 1-9
         for i in range (start,stop):
             # ensure i is greater than or equal to start and not equal to stop to ensure no rows are called twice.
@@ -205,74 +267,26 @@ class create_ins_statements():
                 to_add=",".join((str(Array_ID),str(line[0]),str(line[1]),str(line[2]),str(line[3]),str(line[4]),probename,systematicname,Chromosome,str(line[8]),str(line[9]),str(line[10]),str(line[11]),str(line[12]),str(line[13]),str(line[14]),str(line[15]),str(line[16]),str(line[17]),str(line[18]),str(line[19]),str(line[20]),str(line[21]),str(line[22]),str(line[23]),str(line[24]),str(line[25]),str(line[26]),str(line[27]),str(line[28]),str(line[29]),str(line[30]),str(line[31]),str(line[32]),str(line[33]),str(line[34]),str(line[35]),str(line[36]),str(line[37]),str(line[38]),str(line[39]),str(line[40]),str(line[41]),str(line[42]),str(line[43]),str(line[44])))
                 #No comma at end
                 insstatement=insstatement+"("+to_add+")"
-                #print insstatement    
+                
                 #create a string which is ins and start number - this allows the insert statement to be named for use below
                 ins_number="ins"+ str(start)
                 insnumberforlist=str(ins_number)
-                #print insstatement
-                #print ins_number
+                
                 #Enter the insert statement into the dictionary setup above with key=insnumber and value the sql statement (insstatement)
                 self.insertstatements[ins_number]=insstatement
                 #Add the insert statement name into a list for use below
                 self.insertstatementnames.append(insnumberforlist)
-        
-    
-    #print self.insertstatements
-        
-class run_ins_statements:
-    def run_ins_statements(self,features_listin,arrayID):       
-        # it is quicker to run fewer insert statements so 10 insert statements are created.
-        # create a copy of features array 
-        all_features=list(features_listin)
-            
-        #calculate number of features
-        no_of_probes=len(all_features)
-        #print no_of_probes
-            
-        # use the array_ID that is returned from the insert of feparams.       
-        Array_ID=arrayID
-        #Array_ID=1
-            
-        # using the total number of probes break down into ten subsets. use math.ceil to round up to ensure all probes are included.    
-        subset0=0
-        subset1=int(math.ceil((no_of_probes/10)))
-        subset2=subset1*2
-        subset3=subset1*3
-        subset4=subset1*4
-        subset5=subset1*5
-        subset6=subset1*6
-        subset7=subset1*7
-        subset8=subset1*8
-        subset9=subset1*9
-        
-        #Call the create insert statement class and name it features      
-        featureslooper= create_ins_statements()
-        #featureslooper.create_feat_ins(features_listin,arrayID)
-        
-        #call the looper function within this class and pass it the parameters from the class so that all 10 subgroups have been included
-        featureslooper.looper(subset0,subset1,all_features,Array_ID)
-        featureslooper.looper(subset1,subset2,all_features,Array_ID)
-        featureslooper.looper(subset2,subset3,all_features,Array_ID)
-        featureslooper.looper(subset3,subset4,all_features,Array_ID)
-        featureslooper.looper(subset4,subset5,all_features,Array_ID)
-        featureslooper.looper(subset5,subset6,all_features,Array_ID)
-        featureslooper.looper(subset6,subset7,all_features,Array_ID)
-        featureslooper.looper(subset7,subset8,all_features,Array_ID)
-        featureslooper.looper(subset8,subset9,all_features,Array_ID)
-        featureslooper.looper(subset9,no_of_probes,all_features,Array_ID)
-        insert_features().insert_features(featureslooper.insertstatements,featureslooper.insertstatementnames)
-        
 
-#print  create_ins_statements.create_feat_ins.insertstatementnames
-#create class which executes these SQL statements
 
 class insert_features:
+    #from run_ins_statements give the dictionary of insert statements and list of insert sequence names 
     def insert_features(self,insertstatements,insertstatementnames):
-    #for each insert statement name in the insstatement list
         insertstatements=insertstatements
-        insertstatementnames=insertstatementnames
+        insertstatementnames=insertstatementnames  
         
-        n=1
+        # n is a counter if want to print out progress
+        n=0
+        #for each element (statement name) in the insstatementnames list pull out the corresponding sqlstatement from the dictionary and execute the sql insert 
         for i in insertstatementnames:            
             #connect to db and create cursor
             db=MySQLdb.Connect(host="localhost",port=3307, user ="aled",passwd="aled",db="featextr")
@@ -282,21 +296,25 @@ class insert_features:
                 #print insertstatements[i]
                 cursor3.execute(insertstatements[i])
                 db.commit()
-                print "inserted statement "+str(n)+" of 10"
+                #print "inserted statement " +str(n)+" of 10"
                 n=n+1
             except:
                 db.rollback
                 print "fail - unable to enter feature information"+insertstatements[i]
             db.close
+        
+        #empty all arrays 
         create_ins_statements.insertstatementnames=[]
         create_ins_statements.insertstatements={}
+        extractData.feparams=[]
+        extractData.stats=[]
+        extractData.features=[]
 
+#for each file in the chosenfile array enter this into the feedfile function in extractData class
 files=Getfile.chosenfiles
-print files
 exData=extractData()
 for i in files:
-    print (i)
-    #extractdata(i)
     exData.feedfile(i)
- 
+    print "inserted "+str(i)
+print "all inserted successfully"
  
