6b2fae63a04c4e1a8269f18033b0b9607b8be197
aledj2 <aledjones@nhs.net>
2015-05-22 15:08:22 +0100

    check that all tables in roi exist in db

---
+++ check_analysis_tables_in_ROI.py
@@ -0,0 +1,54 @@
+'''
+Created on 22 May 2015
+
+@author: Aled
+'''
+import MySQLdb
+
+#open connection to database and run SQL select statement
+db=MySQLdb.Connect(host="localhost",port=3307, user ="aled",passwd="aled",db="dev_featextr")
+cursor=db.cursor()
+
+#sql statement
+gettables="""show tables in dev_featextr"""
+
+try:
+    cursor.execute(gettables)
+    gettablesresult=cursor.fetchall()
+except:
+    db.rollback
+    print "fail - unable to retrieve ROI"      
+
+roi_analysis_tables="""select Analysis_table from roi"""
+
+try:
+    cursor.execute(roi_analysis_tables)
+    roi_analysis_tables_result=cursor.fetchall()
+except:
+    db.rollback
+    print "fail - unable to retrieve ROI"      
+
+lenofdbtables=len(gettablesresult)
+len_of_roi_analysis_tables=len(roi_analysis_tables_result)
+
+dbtables=[]
+roi_tables=[]
+
+for i in range(lenofdbtables):
+    a= gettablesresult[i][0]
+    dbtables.append(a)
+for i in range(len_of_roi_analysis_tables):
+    b= roi_analysis_tables_result[i][0]
+    roi_tables.append(b)
+    
+all_is= 0
+for i in roi_tables:
+    if i in dbtables:
+        pass
+    else:
+        all_is=1
+        print str(i)+" does not exist in database"
+        
+db.close
+if all_is == 0:
+    print "all ok"
