61051c8615061beda6e21679ec284d0b7e9c8e77
aledj2 <aledjones@nhs.net>
2015-06-09 09:47:52 +0100

    add reset tables to delete after failed insert

--- reset_tables_after_insert.py
+++ reset_tables_after_insert.py
@@ -5,12 +5,12 @@ Created on 8 Jun 2015
 '''
 import MySQLdb
 
-del1="""DELETE FROM `dev_featextr`.`feparam` WHERE  `Array_ID` > 190;"""
-del2="""DELETE FROM `dev_featextr`.`stats` WHERE  `Array_ID`> 190;"""
-del3="""DELETE FROM `dev_featextr`.`analysis_all` WHERE  `Array_ID`> 190;"""
-del4="""DELETE FROM `dev_featextr`.`target_features2` WHERE  `Array_ID`> 190;"""
-del5="""DELETE FROM `dev_featextr`.`insert_stats` WHERE  `Array_ID`> 190;"""
-del6="""DELETE FROM `dev_featextr`.`shared_imbalances` WHERE  `Array_ID`> 190;"""
+del1="""DELETE FROM `dev_featextr`.`feparam` WHERE  `Array_ID` > 300;"""
+del2="""DELETE FROM `dev_featextr`.`stats` WHERE  `Array_ID`> 300;"""
+del3="""DELETE FROM `dev_featextr`.`analysis_all` WHERE  `Array_ID`> 300;"""
+del4="""DELETE FROM `dev_featextr`.`target_features2` WHERE  `Array_ID`> 300;"""
+del5="""DELETE FROM `dev_featextr`.`insert_stats` WHERE  `Array_ID`> 300;"""
+del6="""DELETE FROM `dev_featextr`.`shared_imbalances` WHERE  `Array_ID`> 300;"""
 
 #create connection
 db=MySQLdb.Connect(host="localhost",port=3307, user ="aled",passwd="aled",db="dev_featextr")
@@ -18,16 +18,28 @@ cursor=db.cursor()
 
 try:
     cursor.execute(del1)
+    db.commit()
+    print "feparam emptied"
     cursor.execute(del2)
+    db.commit()
+    print "stats emptied"
     cursor.execute(del3)
+    db.commit()
+    print "analysis_all emptied"
     cursor.execute(del4)
+    db.commit()
+    print "target_features2 emptied"
     cursor.execute(del5)
+    db.commit()
+    print "insert_stats emptied"
     cursor.execute(del6)
     db.commit()
+    print "shared imbalances emptied"
 except MySQLdb.Error, e:
     db.rollback()
     print "fail" 
     if e[0]!= '###':
         raise
 finally:
+    print "done"
     db.close()
