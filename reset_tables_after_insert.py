3ff787d16bc48255d63ce447d41528f49dfb28e0
aledj2 <aledjones@nhs.net>
2015-09-04 17:00:01 +0100

    Add to reset tables

--- reset_tables_after_insert.py
+++ reset_tables_after_insert.py
@@ -5,7 +5,7 @@ Created on 8 Jun 2015
 '''
 import MySQLdb
 
-arrayID = 1276   # CHECK SYMBOL IN SQL QUERY!
+arrayID = 200   # CHECK SYMBOL IN SQL QUERY!
 
 del1 = """DELETE FROM `feparam` WHERE  `Array_ID`> %s"""
 del2 = """DELETE FROM `stats` WHERE  `Array_ID` > %s """
@@ -13,31 +13,44 @@ del3 = """DELETE FROM `analysis_all` WHERE  `Array_ID` > %s"""
 del4 = """DELETE FROM `features2` WHERE  `Array_ID` > %s"""
 del5 = """DELETE FROM `insert_stats` WHERE  `Array_ID` > %s"""
 del6 = """DELETE FROM `shared_imbalances` WHERE  `Array_ID` > %s"""
-del7 = """DELETE FROM `consecutive_probes_analysis` WHERE  `Array_ID` > %s"""
+del7 = """DELETE FROM `consecutive_probes_analysis` WHERE  `Array_ID` < %s"""
+
+# drop = """ DROP TABLE `features_copy` """
+alter = """ alter table `features` drop index `controltype`, drop index `systematicname`,drop index `array_ID`"""
+create = """ alter table features add index `controltype` (`controltype`), add index `systematicname` (`systematicname`),add index `array_ID` (arrayID), add index ` probekey`(probekey)  """
+
 
 # create connection
 db = MySQLdb.Connect(host="localhost", port=3307, user="aled", passwd="aled", db="dev_featextr")
 cursor = db.cursor()
 
 try:
-    cursor.execute(del1, (arrayID))
-    db.commit()
-    print "feparam emptied"
-    cursor.execute(del2, (arrayID))
-    db.commit()
-    print "stats emptied"
-    cursor.execute(del3, (arrayID))
-    db.commit()
-    print "analysis_all emptied"
-    cursor.execute(del4, (arrayID))
-    db.commit()
-    print "features2 emptied"
-    cursor.execute(del5, (arrayID))
-    db.commit()
-    print "insert_stats emptied"
-    cursor.execute(del6, (arrayID))
-    db.commit()
-    print "shared imbalances emptied"
+    ############################################################################
+    # cursor.execute(alter)
+    # db.commit()
+    # print "indexes dropped"
+    ############################################################################
+    
+    ############################################################################
+    # cursor.execute(del1, (arrayID))
+    # db.commit()
+    # print "feparam emptied"
+    # cursor.execute(del2, (arrayID))
+    # db.commit()
+    # print "stats emptied"
+    # cursor.execute(del3, (arrayID))
+    # db.commit()
+    # print "analysis_all emptied"
+    # cursor.execute(del4, (arrayID))
+    # db.commit()
+    # print "features2 emptied"
+    # cursor.execute(del5, (arrayID))
+    # db.commit()
+    # print "insert_stats emptied"
+    # cursor.execute(del6, (arrayID))
+    # db.commit()
+    # print "shared imbalances emptied"
+    ############################################################################
     cursor.execute(del7, (arrayID))
     db.commit()
     print "consecutive_probes_analysis emptied"
