853c7654445399d3b486d34b4a83f31407044b8f
aledj2 <aledjones@nhs.net>
2015-11-27 12:29:14 +0000

    extend the reset tables script

--- reset_tables_after_insert.py
+++ reset_tables_after_insert.py
@@ -5,15 +5,15 @@ Created on 8 Jun 2015
 '''
 import MySQLdb
 
-arrayID = 1734   # CHECK SYMBOL IN SQL QUERY!
+arrayID = 35   # CHECK SYMBOL IN SQL QUERY!
 
-del1 = """DELETE FROM `feparam` WHERE  `Array_ID`> %s"""
-del2 = """DELETE FROM `stats` WHERE  `Array_ID` > %s """
-del3 = """DELETE FROM `analysis_all` WHERE  `Array_ID` > %s"""
-del4 = """DELETE FROM `features2` WHERE  `Array_ID` >  %s"""
-del5 = """DELETE FROM `insert_stats` WHERE  `Array_ID` > %s"""
-del6 = """DELETE FROM `shared_imbalances` WHERE  `Array_ID` > %s"""
-del7 = """DELETE FROM `consecutive_probes_analysis` WHERE  `Array_ID` > %s"""
+del1 = """DELETE FROM `feparam_mini` WHERE  `Array_ID`= %s"""
+del2 = """DELETE FROM `stats_mini` WHERE  `Array_ID` = %s """
+del3 = """DELETE FROM `analysis_all` WHERE  `Array_ID` = %s"""
+del4 = """DELETE FROM `features_mini` WHERE  `Array_ID` =  %s"""
+del5 = """DELETE FROM `insert_stats` WHERE  `Array_ID` = %s"""
+del6 = """DELETE FROM `shared_imbalances` WHERE  `Array_ID` = %s"""
+del7 = """DELETE FROM `consecutive_probes_analysis` WHERE  `Array_ID` = %s"""
 
 # drop = """ DROP TABLE `features_copy` """
 alter = """ alter table `features` drop index `controltype`, drop index `systematicname`,drop index `array_ID`"""
@@ -46,9 +46,11 @@ try:
     cursor.execute(del5, (arrayID))
     db.commit()
     print "insert_stats emptied"
-    cursor.execute(del6, (arrayID))
-    db.commit()
-    print "shared imbalances emptied"
+    ############################################################################
+    # cursor.execute(del6, (arrayID))
+    # db.commit()
+    # print "shared imbalances emptied"
+    ############################################################################
     cursor.execute(del7, (arrayID))
     db.commit()
     print "consecutive_probes_analysis emptied"
